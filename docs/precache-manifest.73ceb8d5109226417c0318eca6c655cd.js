bf88fcf18ff96d112fdbca645ad0c6d79c4956ce
Eli Mallon <eli@stream.place>
2018-11-19 17:50:36 -0800

    move to 'docs' for github pages

---
+++ docs/precache-manifest.73ceb8d5109226417c0318eca6c655cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e18c30048e64b616daa24163e72932aa",
+    "url": "/static/media/firacode-light-webfont.e18c3004.woff2"
+  },
+  {
+    "revision": "2cb607f24af41faf834bc488c7128db9",
+    "url": "/static/media/firacode-light-webfont.2cb607f2.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32cb1419c683d61497cf",
+    "url": "/static/js/main.32cb1419.chunk.js"
+  },
+  {
+    "revision": "d61dde370cf61a34b72e",
+    "url": "/static/js/1.d61dde37.chunk.js"
+  },
+  {
+    "revision": "32cb1419c683d61497cf",
+    "url": "/static/css/main.f09dc2a2.chunk.css"
+  },
+  {
+    "revision": "191e80156b82f9da5c6a439464890f15",
+    "url": "/index.html"
+  }
+];
